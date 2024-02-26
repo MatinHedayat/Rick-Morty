6805c56d3d6a19a6aa8aa2c16ff48067ce1c89bc
Matin Hedayat <mhmdmatinhedayat@gmail.com>
2024-02-26 21:13:23 +0330

    data about filter queries

---
+++ src/data/filterItems.js
@@ -0,0 +1,12 @@
+export const statusTypes = ['alive', 'dead', 'unknown'];
+export const genderTypes = ['male', 'female', 'unknown'];
+export const speciesTypes = [
+  'Human',
+  'Alien',
+  'Humanoid',
+  'Mythological',
+  'Unknown',
+  'Animal',
+  'Disease',
+  'Robot',
+];
