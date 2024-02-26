e2af748732ef41bff54902958e570368a74e0b6a
Matin Hedayat <mhmdmatinhedayat@gmail.com>
2024-02-26 21:09:51 +0330

    Edit and config more setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      keyframes: {
+        show: {
+          '0%': { opacity: 0, transform: 'translateX(-2rem)' },
+          '100%': { opacity: 100, transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        show: 'show 0.3s ease-in-out 1',
+      },
+    },
   },
+  darkMode: 'class',
   plugins: [],
 };
